325f1b96c0fdcc00a4411fd385b6c1681493b117
Marc <marc.purgimon@gmail.com>
2019-03-21 20:19:41 +0100

    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -10,10 +10,24 @@ Author URI: https://github.com/mapumi1819daw2
 License: GNU GPLv3
 */
 
-function funcio_peupagina($atr,$text) {
+function peu_pagina_per_defecte($atr,$text) {
 	return '<a>Peu de pàgina</a>';
 }
 
-add_shortcode('peupagina', 'funcio_peupagina');
+add_shortcode('peupagina', 'peu_pagina_per_defecte');
+
+function admin_menu_peu_pagina(){
+	$titolPagina="Peu de pàgina per defecte";
+	$titolEntrada="Peu de pàgina per defecte";
+	$role="administrator";
+	$id="peu_pagina_per_defecte";
+	add_menu_page($titolPagina,$titolEntrada, "read",$role,$id); ;
+	}
+
+	add_action('admin_menu','admin_menu_peu_pagina');
+
+
+
+
 
 ?>
